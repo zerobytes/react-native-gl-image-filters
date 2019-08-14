661128c01beca50059af0f36ccde80ee8e485841
Guilherme Ferreira <guilherme.ferreira@zerobyte.app>
2019-08-14 15:04:33 -0300

    ntv

--- src/utils/createGLComponent.js
+++ src/utils/createGLComponent.js
@@ -3,53 +3,52 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default ({
-  displayName,
-  defaultProps,
-  propTypes,
-  frag,
-  receiveValues
+	displayName,
+	defaultProps,
+	propTypes,
+	frag,
+	receiveValues
 }) => {
-  const shaders = GL.Shaders.create({
-    shader: {
-      frag
-    }
-  });
+	const shaders = GL.Shaders.create({
+		shader: {
+			frag
+		}
+	});
 
-  return GL.createComponent(
-    ({ children: t,width, height, ...compProps }) => {
-      const innerProps = {};
-		console.log('INSIDE REACT GL IMAGE')
-      receiveValues.forEach(receive => {
-        Object.keys(compProps).forEach(key => {
-          if (receive === "resolution") {
-            innerProps.resolution = [compProps.width, compProps.height];
-          }
+	return GL.createComponent(
+		({ children: t, width, height, ...compProps }) => {
+			const innerProps = {};
+			console.log('INSIDE REACT GL IMAGE')
+			receiveValues.forEach(receive => {
+				Object.keys(compProps).forEach(key => {
+					if (receive === "resolution") {
+						innerProps.resolution = [compProps.width, compProps.height];
+					}
 
-          if (receive === key) {
-            innerProps[receive] = compProps[key];
-          }
-        });
-      });
-
-      return (
-        <GL.Node
-		  shader={shaders.shader}
-		  width={width}
-		  height={height}
-          uniforms={{ 
-			  t, ...innerProps,
-			  resolution: [width, height],
-			 }}
-        />
-      );
-    },
-    {
-      displayName,
-      defaultProps,
-      propTypes: {
-        children: PropTypes.any.isRequired,
-        ...propTypes
-      }
-    }
-  );
+					if (receive === key) {
+						innerProps[receive] = compProps[key];
+					}
+				});
+			});
+			console.log('PROP',innerProps, compProps)
+			return (
+				<GL.Node
+					shader={shaders.shader}
+					width={compProps.width || 1}
+					height={compProps.height || 1}
+					uniforms={{
+						t, ...innerProps
+					}}
+				/>
+			);
+		},
+		{
+			displayName,
+			defaultProps,
+			propTypes: {
+				children: PropTypes.any.isRequired,
+				...propTypes
+			}
+		}
+	);
 };
